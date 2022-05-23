2a4f3f6d78e7283ffd58e03d5e561f073ad04d33
Boris Skert <boris.skert@gmail.com>
2022-05-23 16:58:52 +0200

    Integrate Wiremock and add a test example

--- src/main/java/de/borisskert/springwiremockexample/GreetingResource.java
+++ src/main/java/de/borisskert/springwiremockexample/GreetingResource.java
@@ -1,15 +1,40 @@
 package de.borisskert.springwiremockexample;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/greeting")
 public class GreetingResource {
 
+    private final WebClient client;
+
+    private final String greetingUrl;
+
+    @Autowired
+    public GreetingResource(
+            WebClient client,
+            @Value("${example.remote.url}") String greetingUrl
+    ) {
+        this.client = client;
+        this.greetingUrl = greetingUrl;
+    }
+
     @GetMapping
     public String get() {
         return "Hello World!";
+    }
+
+    @GetMapping("/remote")
+    public Mono<String> getFromRemote() {
+        return client.get()
+                .uri(greetingUrl)
+                .retrieve()
+                .bodyToMono(String.class);
     }
 }
--- src/test/java/de/borisskert/springwiremockexample/GreetingResourceTest.java
+++ src/test/java/de/borisskert/springwiremockexample/GreetingResourceTest.java
@@ -4,10 +4,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureWebTestClient
+@AutoConfigureWireMock(port = 0)
+@ActiveProfiles("wiremock")
 class GreetingResourceTest {
 
     @Autowired
@@ -20,5 +24,14 @@ class GreetingResourceTest {
                 .exchange()
                 .expectStatus().is2xxSuccessful()
                 .expectBody(String.class).isEqualTo("Hello World!");
+    }
+
+    @Test
+    void shouldResponseWithGreetingFromRemote() throws Exception {
+        client.get()
+                .uri("/greeting/remote")
+                .exchange()
+                .expectStatus().is2xxSuccessful()
+                .expectBody(String.class).isEqualTo("Hello World! from remote");
     }
 }
