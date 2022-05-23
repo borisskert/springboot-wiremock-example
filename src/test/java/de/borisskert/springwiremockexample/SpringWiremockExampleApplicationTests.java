5b53867713fe134778fc61eff96d311f69d8f3c7
Boris Skert <boris.skert@gmail.com>
2022-05-23 13:58:17 +0200

    Create project with Spring initializr

---
+++ src/test/java/de/borisskert/springwiremockexample/SpringWiremockExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package de.borisskert.springwiremockexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWiremockExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
